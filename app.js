892ba381fb9752213c9c821c9f4c3fa6ff932363
coleleg <coleleg@gmail.com>
2021-12-17 09:57:39 -0500

    generates new webpage

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
-var profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+// This was lesson 1 code showing how foreach works
+// const printProfileData = profileDataArr => {
+//     // This...
+//     for (let i = 0; i < profileDataArr.length; i += 1) {
+//       console.log(profileDataArr[i]);
+//     }
+  
+//     console.log('================');
+  
+//     // Is the same as this...
+//     profileDataArr.forEach((profileItem) => {
+//       console.log(profileItem)
+//     });
+//   };
 
-const printProfileData = (profileDataArr) => {
-    for (let i = 0; i < profileDataArr.length; i++)
-    console.log(profileDataArr[i]);
-};
+const fs = require('fs');
 
-printProfileData(profileDataArgs);
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+const generatePage = require('./src/page-template.js');
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
